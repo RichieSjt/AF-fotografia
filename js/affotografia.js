3b6100f728233d02f0c3a8522bbcaefc62eef258
Richie_Sjt <richi252012@hotmail.com>
2021-09-08 20:18:50 -0500

    General updates

--- js/affotografia.js
+++ js/affotografia.js
@@ -4,3 +4,24 @@ var checkExist = setInterval(function() {
        clearInterval(checkExist);
     }
 }, 100)
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd < 10){
+    dd = '0' + dd
+}
+
+if(mm < 10){
+    mm = '0' + mm
+}
+
+today = yyyy + '-' + mm + '-' + dd;
+
+var inputDate = document.getElementById('input-date')
+
+if(inputDate) {
+    inputDate.setAttribute('min', today);
+}
